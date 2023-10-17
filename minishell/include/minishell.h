6a7710439fea1b80e2a2426805b99c80a868a7bd
Farshad Ahmadian Yazdi <f.ahmadiyanyazdi@gmail.com>
2023-10-17 09:33:54 +0200

    Added initial lexer part.

---
+++ minishell/include/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fahmadia <fahmadia@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 14:46:56 by fahmadia          #+#    #+#             */
+/*   Updated: 2023/10/14 08:37:58 by fahmadia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include "typedef.h"
+
+#endif
