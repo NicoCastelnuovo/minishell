4bacd7d83ba44e8f5bae0d76ba4d14a8a67e3a62
Nicolo Castelnuovo <ncasteln@2-G-9.42heilbronn.de>
2023-10-27 08:29:35 +0200

    Before merging to main

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ncasteln <ncasteln@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:20:48 by ncasteln          #+#    #+#             */
-/*   Updated: 2023/10/20 17:04:17 by ncasteln         ###   ########.fr       */
+/*   Updated: 2023/10/27 08:21:18 by ncasteln         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	cd(char *path, char **env)
 
 	if (chdir(path) == -1)
 		return (ENOENT);
-	new_dir = ft_strdup(get_wd(NULL));
+	new_dir = ft_strdup(get_wd());
 	update_env_var("PWD=", new_dir, env);
 	return (0);
 }
